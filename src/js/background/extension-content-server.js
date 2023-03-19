569784263f6869578d10cb4d5fc8ad613fb09b0a
Aleksey Volkov <aleksey_volkov@epam.com>
2023-03-19 13:24:29 +0100

    save lang pair

--- src/js/background/extension-content-server.js
+++ src/js/background/extension-content-server.js
@@ -3,35 +3,52 @@
 import MessageTypes from '../message-types';
 
 
+const LANG_PAIR_STORAGE_KEY = 'langpair'
+const defaultLangPair={
+    sourceLang: chrome.i18n.getUILanguage()==='en'?'es':'en',
+    targetLang: chrome.i18n.getUILanguage()
+};
+
 export default class ExtensionContentServer{
-    _loadLangPair(){
-        let langPair = null;
-        // if (localStorage.langPair){
-        //     try{
-        //         let obj = JSON.parse(localStorage.langPair)
-        //         if(obj.sourceLang && obj.targetLang)
-        //             langPair = obj;
-        //     }
-        //     catch(e){
-        //     }
-        // }
-        return langPair ||
-            {
-                sourceLang: chrome.i18n.getUILanguage()==='en'?'es':'en',
-                targetLang: 'ru'//chrome.i18n.getUILanguage()
-            };
+    _loadLangPair(callback){          
+        chrome.storage.local.get([LANG_PAIR_STORAGE_KEY]).then((result) => {
+            console.log("Value currently is ", result[LANG_PAIR_STORAGE_KEY]);
+            const langPair = result[LANG_PAIR_STORAGE_KEY]??defaultLangPair            
+            callback(langPair)
+        });
+        // let langPair = null;
+        // // if (localStorage.langPair){
+        // //     try{
+        // //         let obj = JSON.parse(localStorage.langPair)
+        // //         if(obj.sourceLang && obj.targetLang)
+        // //             langPair = obj;
+        // //     }
+        // //     catch(e){
+        // //     }
+        // // }
+        // return langPair ||
+        //     {
+        //         sourceLang: chrome.i18n.getUILanguage()==='en'?'es':'en',
+        //         targetLang: 'ru'//chrome.i18n.getUILanguage()
+        //     };
     }
 
     _saveLangPair(langPair){
-        // localStorage.langPair = JSON.stringify(langPair);
+        const objToStore = {}
+        objToStore[LANG_PAIR_STORAGE_KEY] = langPair
+        chrome.storage.local.set(objToStore).then(() => {
+            console.log("Set lang pair to", langPair);
+        });
     }
 
     _onMessage(message, sender, callback){
         switch(message.type){
             case MessageTypes.LoadInitializationData:
-                callback({
-                    langPair: this._loadLangPair()
-                });
+                this._loadLangPair((langPair)=>{
+                    callback({
+                        langPair
+                    });    
+                })
                 break;
             case MessageTypes.SaveLangPair:
                 this._saveLangPair(message.langPair)
