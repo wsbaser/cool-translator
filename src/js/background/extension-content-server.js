498e75a690ed24f9323deabc775867f18e024f6e
Aleksey Volkov <aleksey_volkov@epam.com>
2023-03-19 15:11:20 +0100

    fixed saving lang pair to storage

--- src/js/background/extension-content-server.js
+++ src/js/background/extension-content-server.js
@@ -3,53 +3,29 @@
 import MessageTypes from '../message-types';
 
 
-const LANG_PAIR_STORAGE_KEY = 'langpair'
+const LANG_PAIR_STORAGE_KEY = 'langPair'
 const defaultLangPair={
     sourceLang: chrome.i18n.getUILanguage()==='en'?'es':'en',
-    targetLang: chrome.i18n.getUILanguage()
+    targetLang: chrome.i18n.getUILanguage()==='en'?'en':'es'
 };
 
 export default class ExtensionContentServer{
     _loadLangPair(callback){          
         chrome.storage.local.get([LANG_PAIR_STORAGE_KEY]).then((result) => {
-            console.log("Value currently is ", result[LANG_PAIR_STORAGE_KEY]);
-            const langPair = result[LANG_PAIR_STORAGE_KEY]??defaultLangPair            
-            callback(langPair)
+            const langPair = result[LANG_PAIR_STORAGE_KEY]??defaultLangPair
+            callback({langPair})
         });
-        // let langPair = null;
-        // // if (localStorage.langPair){
-        // //     try{
-        // //         let obj = JSON.parse(localStorage.langPair)
-        // //         if(obj.sourceLang && obj.targetLang)
-        // //             langPair = obj;
-        // //     }
-        // //     catch(e){
-        // //     }
-        // // }
-        // return langPair ||
-        //     {
-        //         sourceLang: chrome.i18n.getUILanguage()==='en'?'es':'en',
-        //         targetLang: 'ru'//chrome.i18n.getUILanguage()
-        //     };
     }
 
     _saveLangPair(langPair){
-        const objToStore = {}
-        objToStore[LANG_PAIR_STORAGE_KEY] = langPair
-        chrome.storage.local.set(objToStore).then(() => {
-            console.log("Set lang pair to", langPair);
-        });
+        chrome.storage.local.set({[LANG_PAIR_STORAGE_KEY]:langPair})
     }
 
     _onMessage(message, sender, callback){
         switch(message.type){
             case MessageTypes.LoadInitializationData:
-                this._loadLangPair((langPair)=>{
-                    callback({
-                        langPair
-                    });    
-                })
-                break;
+                this._loadLangPair(callback)
+                return true
             case MessageTypes.SaveLangPair:
                 this._saveLangPair(message.langPair)
                 break;
